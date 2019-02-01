efb3c078c121d3dd4e5089ccb74b1e031cf075a9
tawachan <tawachan@pixiv.com>
2019-02-02 00:11:56 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
 import React from "react";
-import { drizzleConnect } from "drizzle-react";
 
-const index = props => {
-  const { drizzleStatus, count } = props;
-  if (!drizzleStatus.initialized) {
-    return <div>...loading</div>;
-  }
+const index = () => {
   return (
     <h1>
       <div>this is the index page</div>
-      <div>{count}</div>
     </h1>
   );
 };
 
-function mapStateToProps(state) {
-  return {
-    count: state.count
-  };
-}
-
-export default drizzleConnect(index, mapStateToProps);
+export default index;
